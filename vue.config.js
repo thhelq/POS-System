f7bd41a21f509ad348b8c3bc746ed2856902b2c2
thhelq <thug.lq@qq.com>
2019-08-15 08:32:43 +0800

    修改 vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
     // publicPath: './'
     publicPath: process.env.NODE_ENV === 'production'
         ? '/POS-System/'
-        : '/'
+        : '/',
+        devServer: {
+            https: true
+        }
 }
